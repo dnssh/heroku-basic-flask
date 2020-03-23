6eace1137018ec3255773b9dbf91971711dd5163
Devansh Shah <dnssh936@yahoo.in>
2020-03-23 01:43:03 -0700

    Changed

--- app.py
+++ app.py
@@ -1,17 +1,90 @@
 from flask import Flask
-from datetime import datetime
+import datetime
+
+import json
+import requests
+import argparse
+import logging
+from bs4 import BeautifulSoup
+# from tabulate import tabulate
+import time
+import tweepy
+from configs import *
+
+
 app = Flask(__name__)
+
+FORMAT = '[%(asctime)-15s] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG, filename='bot.log', filemode='a')
+
+S_HEADERS=['Total Cases ','New Cases  ','Deaths       ','New Deaths ','Recovered ','Active Cases','Critical','Cases/Million']
+FILE_NAME = 'corona_india_data.json'
+
+HEADERS = {'Content-type': 'application/json'}
+
+URL1= 'https://www.worldometers.info/coronavirus/'
+
+extract_contents = lambda row: [x.text.replace('\n', '') for x in row]
+
+def slacker(webhook_url=DEFAULT_SLACK_WEBHOOK):
+    def slackit(msg):
+        payload = { 'text': msg }
+        return requests.post(webhook_url, headers=HEADERS, data=json.dumps(payload))
+    return slackit
+
+def tweet(twt):
+    auth =tweepy.OAuthHandler(consumer_key, consumer_secret) 
+    auth.set_access_token(access_token, access_token_secret) 
+    api = tweepy.API(auth)
+    api.update_status(status = twt)
+
+def magic():
+    current_time = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
+
+    print(current_time)
+    response = requests.get(URL1).content
+    soup = BeautifulSoup(response, 'html.parser')
+    header=extract_contents(soup.tr.find_all('th'))
+    data1=soup.find_all("tr",{"total_row"})
+    for row in data1:
+        s = extract_contents(row.find_all('td'))
+
+    stats=s[1:]
+    print(stats)
+
+    res=''
+    for i in range(5):
+        res=res+'\n'+S_HEADERS[i]+' '+stats[i]
+    print(res)
+    twt=u'𝗖𝗼𝗿𝗼𝗻𝗮𝘃𝗶𝗿𝘂𝘀 𝘄𝗼𝗿𝗹𝗱𝘄𝗶𝗱𝗲 𝗹𝗶𝘃𝗲 𝘀𝘁𝗮𝘁𝗶𝘀𝘁𝗶𝗰𝘀 🌎'+res+'\n #coronavirus #covid19 #Hope @who\n'+current_time
+    print("works till here")
+    tweet(twt)
+    print("tweeted")
+
+
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
+	return """
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+    <a href="/dev">button>Hi</button></a>
+    """
+    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    # return """
+    # <h1>Hello heroku</h1>
+    # <p>It is currently {time}.</p>
+
+    # <a href="/dev">button>Hi</button></a>
+    # """.format(time=the_time)
+
+@app.route('/dev')
+def dev():
+    while(True):
+        print("Devuu Rockss")
+        magic()
+        time.sleep(1200)
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
