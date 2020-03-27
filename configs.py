65782adf5f072ce41a06aa86549de381fbbfeede
Devansh Shah <dnssh936@yahoo.in>
2020-03-26 17:50:22 -0700

    Delete configs.py

--- configs.py
+++
@@ -1,6 +0,0 @@
-DEFAULT_SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
-
-consumer_key ="sBC3dCaNgetNdjZ64T19TxqO9"
-consumer_secret ="TNqgXbdvzVqQLbXRMAw3foBatPusHeHL63SpVemGKWlvRcXJiS"
-access_token ="814881202926743553-2dFdPDir0bX8ZGdZZH3SbiF3vBZnQWk"
-access_token_secret ="gRDdA2ZDweh1DadbanbSINc5NiquLxkU3uX5NOA5Y9Ebd"
